98a9d5bcf1722a883e4dccb11bf5b7421e5e823e
devops0014 <106308110+devops0014@users.noreply.github.com>
2023-03-09 08:19:46 +0530

    updtae keypair

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ secret_key = "muJdR/Rs+1ndUPdjizMAuzLJRcUhp85BI6+5cRuv"
 resource "aws_instance" "key" {
 ami = "ami-0aa7d40eeae50c9a9"
 instance_type = "t2.micro"
-key_name = "mykey"
+key_name = "mykeypair"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
 Name = "project-instance"
