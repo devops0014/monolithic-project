9165051496868ee540075489ab726aea78616f5a
devops0014 <106308110+devops0014@users.noreply.github.com>
2023-03-14 10:15:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ secret_key = "1hgqfB6vgmFKds/11Rvr3aXkxOsIaztmbCh4Z2iq"
 resource "aws_instance" "key" {
 ami = "ami-09ba48996007c8b50"
 instance_type = "t2.micro"
-key_name = "mykeypair"
+key_name = "my-project-kp"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
 Name = "project-instance"
