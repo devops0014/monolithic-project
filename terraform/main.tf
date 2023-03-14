78aede6fd2bd2a5e2ae1b8f9de711407c5b53d29
devops0014 <106308110+devops0014@users.noreply.github.com>
2023-03-14 10:17:20 +0530

    update credits

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIARSPNELGYA6SA56P3"
-secret_key = "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"
+access_key = "AKIARSPNELGYF6H52P3Z"
+secret_key = "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"
 }
 
 resource "aws_instance" "key" {
